153d13e5e943a51f7b997052f3a91e0820fe9bd2
Rayhan-Habibi <nahyaribibah2017@gmail.com>
2023-04-23 20:22:02 +0700

    Routines version 2.0 full added new style, deleted text decoration function, deleted clicked function and added button to finish instead click to finish to improve user experience

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import { Icon } from '@iconify/react';
 
 function App() {
  
@@ -14,7 +15,7 @@ function App() {
   const handleClick = (event) => {
     event.preventDefault()
     if (items == "") {
-      window.alert("Please enter a value")
+      window.alert("Task cannot be empty")
     } else {
     const id = todoData.length + 1;
     setTodoData((prev) => [
@@ -27,6 +28,8 @@ function App() {
     ]);
     setItems("");}
   };
+
+  todoData.sort(function (a,b) {return b.id - a.id});
 
   const handleToggle = (id) => {
     let mapped = todoData.map(task => {
@@ -42,24 +45,33 @@ function App() {
       }
     }
   }
-
   return (
-    <div>
-      <h1>Todo List</h1>
+    <div className='flex flex-col'>
+      <h1 className='inline m-5 text-4xl md:mt-20 mb-10 text-center'>Routines</h1>
       <form>
-        <div className="container">
-          <input placeholder='Type your task here!' value={items} onInput={handleChange} type='text'/>
-          <button className='plus' onClick={handleClick}>+</button>
+        <div className='conatiner flex flex-row justify-center my-4 mx-1 md:justify-center md:px-4 '>
+          <input className='md:text-2xl focus:outline-0 w-72 h-10 text-xl border-b-2 pb-6 border-black p-3' placeholder='Type your task here!' value={items} onInput={handleChange} type='text'/>
+          <button className='md:mx-6 md:h-10 md:w-10 w-10 bg-black text-white text-xl ml-5' onClick={handleClick}>+</button>
         </div>
       </form>
-      <div className="listContainer">
-        {
-        todoData.map(
-          (todo) => <p key={todo.id} className={todo.complete? "complete" : ""} onClick={()=>handleToggle(todo.id)} style={{textDecoration: todo.complete? 'line-through' : 'none'}} >{todo.task}</p>
-          )
-        }
+      <div className="flex m-0 justify-center">
+        <div className="Container md:w-96 md:h-96 flex w-11/12 flex-col  h-72 justify-start overflow-y-auto justify-items-strecth">
+          {
+            todoData.map(
+            (todo) => 
+            <div className='flex justify-between'>
+            
+            <p key={todo.id} className={`mx-5 my-2 text-xl`}>{todo.task}</p> 
+            <Icon icon="material-symbols:check-box" width="35" height="35"  className='mr-2 text-gray-300 active:text-gray-700 hover:text-gray-500' />
+            
+            </div>
+            )
+          }
+        </div>
       </div>
-      <button onClick={handleClear}>Click me</button>
+      <div className="flex justify-center">
+        <button className='text-xl shadow-2xl p-2 text-white bg-black w-5/12 md:max-w-xs' onClick={handleClear}>Clear All</button>
+      </div>
     </div>
   );
 }
